6cb7a1898fbc96183ca18debe4c98c12a0e6ed8f
Arthur Lisek-Koper <arthurlisekkoper@gmail.com>
2022-08-30 21:17:47 +0900

    Initial commit frontend
    - All boilerplate code, nothing substantial yet.
    - Using NextJS as the framework with NextUI as the component library

---
+++ frontend/searchle/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import { Button } from '@nextui-org/react';
+
+const Home: NextPage = () => {
+  return (
+    <Button>Click Me!</Button>
+  )
+}
+
+export default Home
