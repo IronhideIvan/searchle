3728a213062b800902ca27d8587cf3b6bd9d7811
Arthur Lisek-Koper <arthurlisekkoper@gmail.com>
2022-09-11 10:46:31 +0900

    Fix event listener being called multiple times
    - The react effect was being called many times (once on each render).
    Added a dependency array to avoid multiple calls to the effect, but it
    was still being called twice on first render. Added a variable to check
    and prevent the second call from occuring.

--- frontend/searchle/components/common/PageRoot.tsx
+++ frontend/searchle/components/common/PageRoot.tsx
@@ -8,17 +8,26 @@ interface Props extends PropsWithChildren {
 }
 
 const PageRootElement = styled("div", {});
+let resizeListenerSet: boolean = false;
 
 const PageRoot = (props: Props) => {
   const [windowHeight, setWindowHeight] = useState<number>(0);
   React.useEffect(() => {
+    // the effect is called twice on startup, so this check
+    // was added in order to avoid stacking listeners for the
+    // same event.
+    if (resizeListenerSet) {
+      return;
+    }
+
     setWindowHeight(window.innerHeight);
     function handleResize() {
       setWindowHeight(window.innerHeight);
     }
 
     window.addEventListener('resize', handleResize)
-  })
+    resizeListenerSet = true;
+  }, [])
 
   return (
     <PageRootElement
